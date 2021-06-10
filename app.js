d19b73e3169c3126d58d8f3f56784edb0aa91071
master1891 <ne.digital91@gmail.com>
2021-06-10 11:30:30 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// const numero = valor => (`El valor es: ${valor}`)
+
+
+// const resultado = numero(30)
+
+// console.log(resultado)
+
+const mascota = {
+	nombre: 'Tom',
+	edad: 3,
+	comidas:[
+		"pescado"
+	]
+}
+
+console.log(mascota)
